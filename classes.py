bf2a52a2f13fac13005ea407012d8a1c2eb220b9
sutha06 <suthakaran.siva0601@gmail.com>
2022-11-04 22:26:32 -0400

    any message

---
+++ classes.py
@@ -0,0 +1,9 @@
+
+class product:
+    def __init__(self, code, name, saleprice, cost, stock, units):
+        self._code = code
+        self._name = name
+        self._saleprice = saleprice
+        self._cost = cost
+        self._stock = stock
+        self._units = units
